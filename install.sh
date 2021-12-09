8cd7ba8eefc8e436504d98c031683503eefb39c5
Maxi Quoß <maxi@quoss.org>
2021-12-09 16:05:17 +0000

    using stow

--- install.sh
+++ install.sh
@@ -20,35 +20,23 @@ cd /opt/yay
 makepkg -si --noconfirm
 
 # install packages
-yay -Sy aom aria2 base-devel cifs-utils curl discord docker docker-compose exfat-utils feh ffmpeg firefox freerdp git gvfs gvfs-nfs gvfs-smb gzip handbrake htop kitty krita lutris man-db man-pages mangohud mediainfo mpv neofetch networkmanager-openvpn nm-connection-editor nmap noto-fonts noto-fonts-cjk noto-fonts-emoji obs-studio openvpn pactl pamixer pavucontrol piper pipewire pipewire-alsa pipewire-jack pipewire-jack-dropin pipewire-media-session pipewire-pulse playerctl python qemu qjackctl rav1e remmina rsync seafile seafile-client signal-desktop smbclient spotify steam svt-av1 telegram-desktop thunar thunderbird tigervnc tmux traceroute ttf-hack unrar unzip vim virt-manager virt-viewer visual-studio-code-bin wget wireguard-tools xorg-server xorg-server-common xorg-xinit xorg-xmodmap xorg-xrandr zsh
+yay -Sy aom aria2 base-devel cifs-utils curl discord docker docker-compose exfat-utils feh ffmpeg firefox freerdp git gvfs gvfs-nfs gvfs-smb gzip handbrake htop kitty krita lutris man-db man-pages mangohud mediainfo mpv neofetch networkmanager-openvpn nm-connection-editor nmap noto-fonts noto-fonts-cjk noto-fonts-emoji obs-studio openvpn pactl pamixer pavucontrol piper pipewire pipewire-alsa pipewire-jack pipewire-jack-dropin pipewire-media-session pipewire-pulse playerctl python qemu qjackctl rav1e remmina rsync seafile seafile-client signal-desktop smbclient spotify steam stow svt-av1 telegram-desktop thunar thunderbird tigervnc tmux traceroute ttf-hack unrar unzip vim virt-manager virt-viewer visual-studio-code-bin wget wireguard-tools xorg-server xorg-server-common xorg-xinit xorg-xmodmap xorg-xrandr zsh
 
 # audio
 systemctl --user enable --now pipewire.service
 systemctl --user enable --now pipewire-pulse.service
 
-# vim
-sudo rm -f /etc/vimrc
-sudo ln -s $SCRIPTPATH/etc/vimrc /etc/vimrc
-
-# xorg
-rm -f ~/.xinitrc
-ln -s $SCRIPTPATH/home/.xinitrc ~/.xinitrc
+# stow
+cd $SCRIPTPATH
+stow dmenu dwm dwm-bar kitty vim xorg
 
 # suckless stuff
-mkdir -p ~/.config
-ln -s $SCRIPTPATH/.config/dwm ~/.config/dwm
-ln -s $SCRIPTPATH/.config/dwm-bar ~/.config/dwm-bar
-ln -s $SCRIPTPATH/.config/dmenu ~/.config/dmenu
 cd ~/.config/dwm
 sudo make clean install
 cd ~/.config/dwm-bar
 sudo make clean install
 cd ~/.config/dmenu
 sudo make clean install
-
-# kitty terminal
-rm -rf ~/.config/kitty/kitty.conf
-ln -s $SCRIPTPATH/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
 # zsh / prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
