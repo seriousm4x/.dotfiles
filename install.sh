a494414fd9f61842be0442f9d344781ad8cc0fb3
Maxi Quoß <maxi@quoss.org>
2021-12-09 19:59:16 +0100

    split packages in base/extra, vbox support

--- install.sh
+++ install.sh
@@ -5,6 +5,15 @@ if [ "$EUID" -eq 0 ]
     exit
 fi
 
+while true; do
+    read -p "Install extra packages? [y]/n" yn
+    case $yn in
+        [Yy]* ) extraPkg="y"; break;;
+        [Nn]* ) extraPkg="n";;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 # set script path
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
@@ -14,20 +23,27 @@ sudo pacman -Syu --noconfirm
 
 # yay
 sudo mkdir -p /opt/yay
-sudo chown $(whoami). /opt/yay
+sudo chown "$(whoami)". /opt/yay
 git clone https://aur.archlinux.org/yay.git /opt/yay
 cd /opt/yay
 makepkg -si --noconfirm
 
 # install packages
-yay -Sy aom aria2 base-devel cifs-utils curl discord docker docker-compose exfat-utils feh ffmpeg firefox freerdp git gvfs gvfs-nfs gvfs-smb gzip handbrake htop kitty krita lutris man-db man-pages mangohud mediainfo mpv neofetch networkmanager-openvpn nm-connection-editor nmap noto-fonts noto-fonts-cjk noto-fonts-emoji obs-studio openvpn pactl pamixer pavucontrol piper pipewire pipewire-alsa pipewire-jack pipewire-jack-dropin pipewire-media-session pipewire-pulse playerctl python qemu qjackctl rav1e remmina rsync seafile seafile-client signal-desktop smbclient spotify steam stow svt-av1 telegram-desktop thunar thunderbird tigervnc tmux traceroute ttf-hack unrar unzip vim virt-manager virt-viewer visual-studio-code-bin wget wireguard-tools xorg-server xorg-server-common xorg-xinit xorg-xmodmap xorg-xrandr zsh
+yay -Sy - < base.txt
+if [ $extraPkg == "y" ]; then
+    yay -Sy - < extra.txt
+    systemctl --user enable --now pipewire.service
+    systemctl --user enable --now pipewire-pulse.service
+fi
 
-# audio
-systemctl --user enable --now pipewire.service
-systemctl --user enable --now pipewire-pulse.service
+if [ $(systemd-detect-virt) == "oracle" ]; then
+    yay -Sy virtualbox-guest-utils
+    sudo systemctl enable --now vboxservice.service
+    VBoxClient-all
+fi
 
 # stow
-cd $SCRIPTPATH
+cd "$SCRIPTPATH"
 stow dmenu dwm dwm-bar kitty vim xorg
 
 # suckless stuff
@@ -43,9 +59,10 @@ ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
 ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
 ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
 ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
-curl 'https://gist.githubusercontent.com/seriousm4x/b93b2af2c226b82d755309f87ef936d3/raw/5cb4fd37b7984aed61997a157429cc032b90b297/.zpreztorc' -o ~/.zpreztorc
-echo 'export PATH=$PATH:/home/max/.local/bin' >> ~/.zshrc
+curl "https://gist.githubusercontent.com/seriousm4x/b93b2af2c226b82d755309f87ef936d3/raw/5cb4fd37b7984aed61997a157429cc032b90b297/.zpreztorc" -o ~/.zpreztorc
+echo "export PATH=$PATH:/home/max/.local/bin" >> ~/.zshrc
 echo 'alias ssh="kitty +kitten ssh"' >> ~/.zshrc
+echo 'alias ll="exa -lah"' >> ~/.zshrc
 chsh -s /bin/zsh
 
 # startx at login
