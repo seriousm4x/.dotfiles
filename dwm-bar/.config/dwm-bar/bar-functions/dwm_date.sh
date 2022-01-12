8843b956b1a86d0152e7047f6d9fb05b324115f5
Maxi Quoß <maxi@quoss.org>
2022-01-13 00:13:38 +0100

    statusbar date format

--- dwm-bar/.config/dwm-bar/bar-functions/dwm_date.sh
+++ dwm-bar/.config/dwm-bar/bar-functions/dwm_date.sh
@@ -8,9 +8,9 @@
 dwm_date () {
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "📆 %s" "$(date "+%a %d-%m-%y %T")"
+        printf "📆 %s" "$(date "+%a, %d.%m.%Y, %T")"
     else
-        printf "DAT %s" "$(date "+%a %d-%m-%y %T")"
+        printf "DAT %s" "$(date "+%a, %d.%m.%Y, %T")"
     fi
     printf "%s\n" "$SEP2"
 }
