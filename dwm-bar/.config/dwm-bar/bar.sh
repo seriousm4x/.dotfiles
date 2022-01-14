2fe64b2e6f0757570045701ad5d0ab21ee2730d5
Maxi Quoß <maxi@quoss.org>
2022-01-14 01:31:03 +0100

    replace bar with custom script and fontawesome

---
+++ dwm-bar/.config/dwm-bar/bar.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SEP="|"
+
+bar_vpn () {
+    nm_vpn=$(nmcli -a | grep 'VPN connection' | sed -e 's/\( VPN connection\)*$//g')
+    wg_vpn=$(nmcli connection | grep 'wireguard' | sed 's/\s.*$//')
+    VPN="" 
+    if [ -n "$nm_vpn" ]; then
+        VPN=$nm_vpn
+    fi
+    if [ -n "$wg_vpn" ] && [ -n "$VPN" ]; then
+        VPN="${VPN} $wg_vpn"
+    elif [ -n "$wg_vpn" ] && [ -z "$VPN" ]; then
+        VPN=$wg_vpn
+    fi
+    if [ "$VPN" != "" ]; then
+        printf " %s" "$VPN $SEP "
+    fi
+}
+
+bar_mem () {
+    printf " $(free -h | grep Mem | awk '{print $3}') $SEP "
+}
+
+bar_cpu () {
+    printf " $(top -bn1 | grep Cpu | awk '{print $2}')%% $SEP "
+}
+
+bar_vol () {
+    VOL=$(pamixer --get-volume)
+    MUTED=$(pamixer --get-mute)
+    if [ "$MUTED" = "true" ] || [ "$VOL" -eq 0 ]; then
+        printf " $SEP "
+    elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 50 ]; then
+        printf " $VOL%% $SEP "
+    else
+        printf " $VOL%% $SEP "
+    fi
+}
+
+bar_date () {
+    printf " %s" "$(date "+%a, %d.%m.%Y, %T") $SEP"
+}
+
+while true; do
+    xsetroot -name "$(bar_vpn)$(bar_mem)$(bar_cpu)$(bar_vol)$(bar_date)"
+    sleep 1
+done
--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -8,7 +8,7 @@ static const unsigned int gappx     = 10;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=12", "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Hack:size=12", "FontAwesome:size=12:antialias=true" };
 static const char dmenufont[]       = "Hack:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
