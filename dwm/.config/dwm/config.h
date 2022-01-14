4b34dd6b8ba7b47e607680570aa6cb8ac99879fa
Maxi Quoß <maxi@quoss.org>
2022-01-14 21:00:35 +0100

    better tags

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -37,16 +37,20 @@ static const int systraypinningfailfirst 	= 1;   /* 1: if pinning fails, display
 static const int showsystray        		= 1;     /* 0 means no systray */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      		instance    	title       tags mask     isfloating   monitor */
+	{ "firefox",  		NULL,			NULL,       1 << 0,       0,           0 },
+	{ "kitty",			NULL,			NULL,       1 << 1,       0,           -1 },
+	{ "Code",	 	 	NULL,			NULL,       1 << 2,       0,           0 },
+	{ "Thunar",			NULL,			NULL,       1 << 3,       0,           0 },
+	{ "Thunderbird",	NULL,			NULL,       1 << 4,       0,           0 },
+	{ "discord",		NULL,			NULL,       1 << 5,       0,           1 },
 };
 
 /* layout(s) */
